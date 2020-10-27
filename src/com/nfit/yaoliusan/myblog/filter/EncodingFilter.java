bfce536bcc83431ede1bee358173bf2e328d070f
yaoliusan <yaoliusan@gmail.com>
2020-10-27 16:46:16 +0800

    add jquery and bootstrap cdn

--- src/com/nfit/yaoliusan/myblog/filter/EncodingFilter.java
+++ src/com/nfit/yaoliusan/myblog/filter/EncodingFilter.java
@@ -2,14 +2,19 @@ package com.nfit.yaoliusan.myblog.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @WebFilter("/*")
 public class EncodingFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		request.setCharacterEncoding("utf-8");
-		response.setCharacterEncoding("utf-8");
+		final HttpServletRequest req = (HttpServletRequest) request;
+
+		if (!req.getRequestURI().matches("/html/|/img/")) {
+			request.setCharacterEncoding("utf-8");
+			response.setCharacterEncoding("utf-8");
+		}
 
 		chain.doFilter(request, response);
 	}
